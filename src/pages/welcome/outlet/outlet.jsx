c53fbf186393dccfc6b395b75b0c8c0964818a4d
Alexsama <alex@pacomsolution.com>
2022-10-18 19:52:41 +0800

    fix error name

--- src/pages/welcome/outlet/outlet.jsx
+++ src/pages/welcome/outlet/outlet.jsx
@@ -137,7 +137,7 @@ class Outlet extends Component {
             email: '',
             phone: '',
         }
-        if (this.state.userData.name.split(' ',2).length > 1) {
+        if (!this.state.userData.name.split(' ',2).length > 1) {
             status.name = warningstatus
             hasError = true
         }
